355d0729b37458aa5b661d7be18ae4630a3da2c0
Ihtml <2483690020@qq.com>
2018-12-18 22:59:01 +0800

    optimize

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -15,8 +15,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # 创建一颗子弹， 并将其加入到编组bullets中
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     # 响应松开
@@ -45,7 +46,7 @@ def update_screen(ai_settings, screen, ship, bullets):
     # 在飞船和外星人后面重绘所有子弹
     for bullet in bullets.sprites():
         bullet.draw_bullet()
-        
+
     ship.blitme()
 
     # 让最近绘制的屏幕可见

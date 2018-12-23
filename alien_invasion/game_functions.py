14a7d59775e8aa2e3b65efd8872314f79733b17d
Ihtml <2483690020@qq.com>
2018-12-23 23:59:19 +0800

    play game

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -38,11 +38,15 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     # 监视键盘和鼠标事件
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y == pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
 
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ai_settings, screen, ship, bullets)
@@ -50,6 +54,10 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    # 在玩家单击Play按钮时开始新游戏
+    if play_button.rect.collisions(mouse_x, mouse_y):
+        stats.game_active = True
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     # 每次循环时都重绘屏幕
